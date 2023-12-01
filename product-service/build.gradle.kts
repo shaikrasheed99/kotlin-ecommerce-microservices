10890aa064a61fbe62ce283507e191a3d72a3117
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-12-01 23:20:36 +0530

    [Product-service] Add. Postgresql database configuration

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -20,8 +20,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
