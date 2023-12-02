2dabb433faa7d7337ae8e37c7cfb60bcdddb523f
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-12-02 18:43:01 +0530

    [Product-service] Add. Test cases for create new product api

---
+++ product-service/src/test/kotlin/com/ecommerce/productservice/controllers/ProductControllerTest.kt
@@ -0,0 +1,110 @@
+package com.ecommerce.productservice.controllers
+
+import com.ecommerce.productservice.constants.MessageResponses
+import com.ecommerce.productservice.constants.StatusResponses
+import com.ecommerce.productservice.dto.requests.ProductRequestBody
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.math.BigDecimal
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureMockMvc
+@Testcontainers
+internal class ProductControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    companion object {
+        @Container
+        @ServiceConnection
+        private val postgreSQLContainer = PostgreSQLContainer("postgres:latest")
+            .withDatabaseName("products")
+            .withInitScript("create-products-table.sql")
+    }
+
+    @Test
+    internal fun shouldBeAbleToCreateNewProduct() {
+        val productRequestBodyJson = ProductRequestBody(
+            name = "test name",
+            description = "test description",
+            price = BigDecimal(10.20)
+        ).let {
+            ObjectMapper().writeValueAsString(it)
+        }
+
+        mockMvc.perform(
+            post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestBodyJson)
+        ).andExpect(status().isOk)
+            .andExpect(jsonPath("$.status").value(StatusResponses.SUCCESS.name))
+            .andExpect(jsonPath("$.code").value(HttpStatus.OK.name))
+            .andExpect(jsonPath("$.message").value(MessageResponses.PRODUCT_CREATION_SUCCESS.message))
+    }
+
+    @Test
+    internal fun shouldNotBeAbleToCreateNewProductWhenNameIsBlank() {
+        val productRequestBodyJson = ProductRequestBody(
+            name = "",
+            description = "test description",
+            price = BigDecimal(10.20)
+        ).let {
+            ObjectMapper().writeValueAsString(it)
+        }
+
+        mockMvc.perform(
+            post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestBodyJson)
+        ).andExpect(status().isBadRequest)
+    }
+
+    @Test
+    internal fun shouldNotBeAbleToCreateNewProductWhenDescriptionIsBlank() {
+        val productRequestBodyJson = ProductRequestBody(
+            name = "test name",
+            description = "",
+            price = BigDecimal(10.20)
+        ).let {
+            ObjectMapper().writeValueAsString(it)
+        }
+
+        mockMvc.perform(
+            post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestBodyJson)
+        ).andExpect(status().isBadRequest)
+    }
+
+    @Test
+    internal fun shouldNotBeAbleToCreateNewProductWhenPriceIsNegative() {
+        val productRequestBodyJson = ProductRequestBody(
+            name = "test name",
+            description = "test description",
+            price = BigDecimal(-10.20)
+        ).let {
+            ObjectMapper().writeValueAsString(it)
+        }
+
+        mockMvc.perform(
+            post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productRequestBodyJson)
+        ).andExpect(status().isBadRequest)
+    }
+}
---
+++ product-service/src/test/kotlin/com/ecommerce/productservice/services/ProductServiceTest.kt
@@ -0,0 +1,45 @@
+package com.ecommerce.productservice.services
+
+import com.ecommerce.productservice.dto.requests.ProductRequestBody
+import com.ecommerce.productservice.models.Product
+import com.ecommerce.productservice.models.ProductRepository
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import java.math.BigDecimal
+
+@ExtendWith(MockitoExtension::class)
+internal class ProductServiceTest {
+    @Mock
+    private lateinit var productRepository: ProductRepository
+
+    @InjectMocks
+    private lateinit var productService: ProductService
+
+    @Test
+    internal fun shouldBeAbleToCreateNewProduct() {
+        val productRequestBody = ProductRequestBody(
+            name = "test name",
+            description = "test description",
+            price = BigDecimal(10.20)
+        )
+
+        val product = Product(
+            id = 1,
+            name = "test name",
+            description = "test description",
+            price = BigDecimal(10.20)
+        )
+
+        `when`(productRepository.save(any(Product::class.java))).thenReturn(product)
+
+        assertDoesNotThrow { productService.createProduct(productRequestBody) }
+
+        verify(productRepository, times(1)).save(any(Product::class.java))
+    }
+}
