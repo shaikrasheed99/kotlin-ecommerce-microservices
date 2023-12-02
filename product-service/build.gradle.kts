7e47c49593b122cb340eac6c7d0242ce62795416
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-12-02 18:42:10 +0530

    [Product-service] Add. Testcontainers dependency

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
