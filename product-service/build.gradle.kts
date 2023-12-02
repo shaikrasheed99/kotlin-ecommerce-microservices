5dbc0881bd95fe1079860b8df7a50059172ea546
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-12-02 17:43:29 +0530

    [Product-service] Add. Validations to product request body attributes

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- product-service/src/main/kotlin/com/ecommerce/productservice/controllers/ProductController.kt
+++ product-service/src/main/kotlin/com/ecommerce/productservice/controllers/ProductController.kt
@@ -5,6 +5,7 @@ import com.ecommerce.productservice.dto.requests.ProductRequestBody
 import com.ecommerce.productservice.dto.response.SuccessResponse
 import com.ecommerce.productservice.services.ProductService
 import com.ecommerce.productservice.utils.createSuccessResponse
+import jakarta.validation.Valid
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,7 +19,7 @@ class ProductController(private val productService: ProductService) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     @PostMapping
-    fun createProduct(@RequestBody productRequestBody: ProductRequestBody): ResponseEntity<SuccessResponse> {
+    fun createProduct(@Valid @RequestBody productRequestBody: ProductRequestBody): ResponseEntity<SuccessResponse> {
         val newProduct = productService.createProduct(productRequestBody)
 
         val message = MessageResponses.PRODUCT_CREATION_SUCCESS.message
