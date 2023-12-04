ca246b8afbc6ac784ce1787efff04c755bb0e1ed
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-12-04 13:40:39 +0530

    [Inventory-service] Add. Postgresql database configuration

--- inventory-service/build.gradle.kts
+++ inventory-service/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.20"
 	kotlin("plugin.spring") version "1.9.20"
+	kotlin("plugin.jpa") version "1.9.20"
 }
 
 group = "com.ecommerce"
@@ -22,6 +23,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
