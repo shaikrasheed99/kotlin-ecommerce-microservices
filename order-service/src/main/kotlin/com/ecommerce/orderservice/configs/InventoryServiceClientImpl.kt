78e69aa348b4720c7a44b70272e521521cac4963
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-12-09 14:56:20 +0530

    [Order-service] Add. Env variable for inventory-service base url

--- order-service/src/main/kotlin/com/ecommerce/orderservice/configs/InventoryServiceClientImpl.kt
+++ order-service/src/main/kotlin/com/ecommerce/orderservice/configs/InventoryServiceClientImpl.kt
@@ -1,6 +1,7 @@
 package com.ecommerce.orderservice.configs
 
 import com.ecommerce.orderservice.dto.responses.Response
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
@@ -8,10 +9,15 @@ import org.springframework.web.reactive.function.client.WebClient
 class InventoryServiceClientImpl(
     private val webClientBuilder: WebClient.Builder
 ) : InventoryServiceClient {
+    @Value("\${inventory.service.url}")
+    private lateinit var inventoryServiceBaseUrl: String
+
     override fun getInventoryBySkuCode(skuCode: String): Response? {
+        val url = "$inventoryServiceBaseUrl/inventory/{sku-code}"
+
         return webClientBuilder.build()
             .get()
-            .uri("http://inventory-service/inventory/{sku-code}", skuCode)
+            .uri(url, skuCode)
             .retrieve()
             .bodyToMono(com.ecommerce.orderservice.dto.responses.Response::class.java)
             .block()
