85518b131e4959df845973e90f1e76225da6a067
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-01-24 15:27:47 +0530

    [Product-service] Add. Annotation tests for product controller

--- product-service/src/test/kotlin/com/ecommerce/productservice/unittests/controllers/ProductControllerTest.kt
+++ product-service/src/test/kotlin/com/ecommerce/productservice/unittests/controllers/ProductControllerTest.kt
@@ -4,20 +4,39 @@ import com.ecommerce.productservice.constants.MessageResponses
 import com.ecommerce.productservice.controllers.ProductController
 import com.ecommerce.productservice.exceptions.ProductNotFound
 import com.ecommerce.productservice.services.ProductService
+import com.ecommerce.productservice.utils.EntityUtils.getMethodAnnotations
 import com.ecommerce.productservice.utils.TestUtils.assertCommonFields
 import com.ecommerce.productservice.utils.TestUtils.createProduct
 import com.ecommerce.productservice.utils.TestUtils.createProductRequestBody
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldNotBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 class ProductControllerTest : DescribeSpec({
     val mockProductService = mockk<ProductService>()
     val productController = ProductController(mockProductService)
 
     val product = createProduct()
+
+    describe("Product Controller - annotations") {
+        it("should have relevant annotations to the Product Controller class") {
+            val classAnnotations = productController.javaClass.annotations
+            val restControllerAnnotation = classAnnotations.firstOrNull { it is RestController } as RestController
+            val requestMappingAnnotation = classAnnotations.firstOrNull { it is RequestMapping } as RequestMapping
+
+            restControllerAnnotation shouldNotBe null
+
+            requestMappingAnnotation shouldNotBe null
+            requestMappingAnnotation.value.firstOrNull { it == "/products" } shouldNotBe null
+        }
+    }
 
     describe("Create New Product") {
         it("should be able to Create New Product") {
@@ -39,6 +58,15 @@ class ProductControllerTest : DescribeSpec({
             verify {
                 mockProductService.createProduct(productRequestBody)
             }
+        }
+    }
+
+    describe("Create New Product - annotations") {
+        it("should have PostMapping annotation to the createProduct method") {
+            val annotations = productController.getMethodAnnotations("createProduct")
+            val postMappingAnnotation = annotations.firstOrNull { it is PostMapping } as PostMapping
+
+            postMappingAnnotation shouldNotBe null
         }
     }
 
@@ -89,6 +117,15 @@ class ProductControllerTest : DescribeSpec({
         }
     }
 
+    describe("Get all Products - annotations") {
+        it("should have GetMapping annotation to the getAllProducts method") {
+            val annotations = productController.getMethodAnnotations("getAllProducts")
+            val getMappingAnnotation = annotations.firstOrNull { it is GetMapping } as GetMapping
+
+            getMappingAnnotation shouldNotBe null
+        }
+    }
+
     describe("Get Product by Id") {
         it("should be able to return Product by Id") {
             every { mockProductService.getProductBy(product.id!!) } returns product
@@ -117,6 +154,16 @@ class ProductControllerTest : DescribeSpec({
             verify {
                 mockProductService.getProductBy(product.id!!)
             }
+        }
+    }
+
+    describe("Get Product by Id - annotations") {
+        it("should have GetMapping annotation with path value to the getProductById method") {
+            val annotations = productController.getMethodAnnotations("getProductById")
+            val getMappingAnnotation = annotations.firstOrNull { it is GetMapping } as GetMapping
+
+            getMappingAnnotation shouldNotBe null
+            getMappingAnnotation.value.firstOrNull { it == "/{id}" } shouldNotBe null
         }
     }
 })
