7704709e54d197414084d5740eb3c75af8ba12ce
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-01-24 16:23:48 +0530

    [Product-service] Add. Annotation tests for health controller

--- product-service/src/test/kotlin/com/ecommerce/productservice/unittests/controllers/HealthControllerTest.kt
+++ product-service/src/test/kotlin/com/ecommerce/productservice/unittests/controllers/HealthControllerTest.kt
@@ -3,11 +3,15 @@ package com.ecommerce.productservice.unittests.controllers
 import com.ecommerce.productservice.constants.MessageResponses
 import com.ecommerce.productservice.constants.StatusResponses
 import com.ecommerce.productservice.controllers.HealthController
+import com.ecommerce.productservice.utils.EntityUtils.getMethodAnnotations
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-class HealthControllerTest(): DescribeSpec({
+class HealthControllerTest() : DescribeSpec({
     val healthController = HealthController()
 
     describe("Health Controller") {
@@ -18,6 +22,23 @@ class HealthControllerTest(): DescribeSpec({
             responseBody?.code shouldBe HttpStatus.OK
             responseBody?.message shouldBe MessageResponses.SERVER_UP.message
             responseBody?.data shouldBe null
+        }
+    }
+
+    describe("Health Controller - annotations") {
+        it("should have RestController annotation to the Health Controller class") {
+            val classAnnotations = healthController.javaClass.annotations
+            val restControllerAnnotation = classAnnotations.firstOrNull { it is RestController } as RestController
+
+            restControllerAnnotation shouldNotBe null
+        }
+
+        it("should have GetMapping annotation to the health method") {
+            val annotations = healthController.getMethodAnnotations("health")
+            val getMappingAnnotation = annotations.firstOrNull { it is GetMapping } as GetMapping
+
+            getMappingAnnotation shouldNotBe null
+            getMappingAnnotation.value.firstOrNull { it == "/health" } shouldNotBe null
         }
     }
 })
