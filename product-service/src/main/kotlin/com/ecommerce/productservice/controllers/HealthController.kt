1db2411f5f02143fa4cb6ba48850eb6bb70362d4
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-01-24 22:58:28 +0530

    [Product-service] Add. Logger tests in health controller

--- product-service/src/main/kotlin/com/ecommerce/productservice/controllers/HealthController.kt
+++ product-service/src/main/kotlin/com/ecommerce/productservice/controllers/HealthController.kt
@@ -3,6 +3,7 @@ package com.ecommerce.productservice.controllers
 import com.ecommerce.productservice.constants.MessageResponses
 import com.ecommerce.productservice.constants.StatusResponses
 import com.ecommerce.productservice.dto.response.Response
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthController {
-    private val logger = LoggerFactory.getLogger(this::class.java)
-
     @GetMapping("/health")
     fun health(): ResponseEntity<Response> {
         val message = MessageResponses.SERVER_UP.message
@@ -26,5 +25,9 @@ class HealthController {
         logger.info(message)
 
         return ResponseEntity.ok(response)
+    }
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(this::class.java)
     }
 }
--- product-service/src/test/kotlin/com/ecommerce/productservice/unittests/controllers/HealthControllerTest.kt
+++ product-service/src/test/kotlin/com/ecommerce/productservice/unittests/controllers/HealthControllerTest.kt
@@ -7,6 +7,19 @@ import com.ecommerce.productservice.utils.EntityUtils.getMethodAnnotations
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.mockk.clearAllMocks
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockkObject
+import io.mockk.runs
+import io.mockk.slot
+import io.mockk.spyk
+import io.mockk.unmockkObject
+import io.mockk.verify
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -39,6 +52,12 @@ class HealthControllerTest : DescribeSpec({
 
             getMappingAnnotation shouldNotBe null
             getMappingAnnotation.value.firstOrNull { it == "/health" } shouldNotBe null
+        }
+    }
+
+    describe("Health Controller - logger") {
+        it("should initialize the logger of health controller") {
+            HealthController.logger shouldNotBe null
         }
     }
 })
