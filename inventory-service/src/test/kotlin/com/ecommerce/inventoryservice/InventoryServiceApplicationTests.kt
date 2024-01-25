a8ae80d177b72b94ac41c53bec85d11bc8fe95f5
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-01-25 23:27:38 +0530

    [Inventory-service] Fix. Integration tests

--- inventory-service/src/test/kotlin/com/ecommerce/inventoryservice/InventoryServiceApplicationTests.kt
+++ inventory-service/src/test/kotlin/com/ecommerce/inventoryservice/InventoryServiceApplicationTests.kt
@@ -2,9 +2,22 @@ package com.ecommerce.inventoryservice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
+@Testcontainers
 class InventoryServiceApplicationTests {
+
+	companion object {
+		@Container
+		@ServiceConnection
+		private val postgreSQLContainer = PostgreSQLContainer("postgres:latest")
+			.withDatabaseName("inventory")
+			.withInitScript("create-inventory-table.sql")
+	}
 
 	@Test
 	fun contextLoads() {
--- inventory-service/src/test/kotlin/com/ecommerce/inventoryservice/integrationtests/HealthControllerIntegrationTest.kt
+++ inventory-service/src/test/kotlin/com/ecommerce/inventoryservice/integrationtests/HealthControllerIntegrationTest.kt
@@ -6,17 +6,30 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 internal class HealthControllerIntegrationTest {
     @Autowired
     private lateinit var mockMvc: MockMvc;
+
+    companion object {
+        @Container
+        @ServiceConnection
+        private val postgreSQLContainer = PostgreSQLContainer("postgres:latest")
+            .withDatabaseName("inventory")
+            .withInitScript("create-inventory-table.sql")
+    }
 
     @Test
     internal fun shouldBeAbleToReturnSuccessResponseWhenServerIsUp() {
