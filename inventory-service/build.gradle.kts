ef16171c6d573f4cce31adfdd290741eec612cbb
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-01-25 12:10:06 +0530

    [Inventory-service] Add. Kotest and Mockk dependencies

--- inventory-service/build.gradle.kts
+++ inventory-service/build.gradle.kts
@@ -30,7 +30,14 @@ dependencies {
 
 	runtimeOnly("org.postgresql:postgresql")
 
+	testImplementation("io.mockk:mockk:1.13.9")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.kotest:kotest-assertions-json:4.6.3")
+	testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
+
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
