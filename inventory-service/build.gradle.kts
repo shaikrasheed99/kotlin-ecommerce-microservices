c677a748b7bb46f9e0c6e81df028cbb20e843e85
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-01-25 12:07:01 +0530

    [Inventory-service] Add. Jacoco test coverage plugin

--- inventory-service/build.gradle.kts
+++ inventory-service/build.gradle.kts
@@ -3,9 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.4"
+
 	kotlin("jvm") version "1.9.20"
 	kotlin("plugin.spring") version "1.9.20"
 	kotlin("plugin.jpa") version "1.9.20"
+	jacoco
 }
 
 group = "com.ecommerce"
@@ -25,7 +27,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.3")
+
 	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
@@ -41,4 +45,67 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jacoco {
+	toolVersion = "0.8.9"
+	reportsDirectory.set(layout.buildDirectory.dir("jacoco"))
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	classDirectories.setFrom(
+		sourceSets.main.get().output.asFileTree.matching {
+			exclude(
+				"**/ProductServiceApplication*.*",
+				"**/ProductsTableSchemaCreation*.*"
+			)
+		}
+	)
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
+	}
+}
+
+tasks.jacocoTestCoverageVerification {
+	dependsOn(tasks.test)
+	val minimumThreadValue = 0.99.toBigDecimal()
+	violationRules {
+		rule {
+			element = "CLASS"
+			classDirectories.setFrom(
+				sourceSets.main.get().output.asFileTree.matching {
+					exclude(
+						"**/ProductServiceApplication*.*",
+						"**/ProductsTableSchemaCreation*.*"
+					)
+				}
+			)
+			limit {
+				counter = "LINE"
+				minimum = minimumThreadValue
+			}
+			limit {
+				counter = "BRANCH"
+				minimum = minimumThreadValue
+			}
+			limit {
+				counter = "METHOD"
+				minimum = minimumThreadValue
+			}
+			limit {
+				counter = "CLASS"
+				minimum = minimumThreadValue
+			}
+			limit {
+				counter = "COMPLEXITY"
+				minimum = minimumThreadValue
+			}
+		}
+	}
 }
