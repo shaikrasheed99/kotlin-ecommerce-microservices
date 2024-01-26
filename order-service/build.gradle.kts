6faa0368bd515fd9fd08ea467484eb17694672b0
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-01-26 21:26:00 +0530

    [Order-service] Add. Kotest and Mockk dependencies

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -4,6 +4,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.4"
+
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
     kotlin("plugin.jpa") version "1.9.20"
@@ -32,12 +33,20 @@ dependencies {
     implementation("org.mock-server:mockserver-netty:5.15.0")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.3")
     implementation("com.h2database:h2")
+
     runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("io.mockk:mockk:1.13.9")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.kotest:kotest-assertions-json:4.6.3")
+
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
-    testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.testcontainers:mockserver:1.19.3")
 }
 
