40aa102abea24faf583db76f4c01da8fc9fcf102
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-02-04 22:09:43 +0530

    [API-Gateway] Refactor. Separate common dependencies

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -22,8 +22,10 @@ extra["springCloudVersion"] = "2023.0.0"
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.3")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
