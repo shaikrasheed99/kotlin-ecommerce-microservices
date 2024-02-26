c2d46fd2ce79ff772ba3834d1d785cb1d478ab7e
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-02-26 22:56:53 +0530

    [Notification-service] Add. Unit tests for order placed events

---
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/unittests/events/OrderPlacedEventTest.kt
@@ -0,0 +1,38 @@
+package com.ecommerce.notificationservice.unittests.events
+
+import com.ecommerce.notificationservice.events.OrderPlacedEvent
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.util.UUID
+
+class OrderPlacedEventTest : DescribeSpec({
+    describe("OrderPlacedEvent - getters") {
+        it("should have all the attributes in the response") {
+            val orderPlacedEvent = createTestOrderPlacedEvent()
+
+            orderPlacedEvent.id shouldBe UUID.fromString("aaa8a937-0504-4468-823a-04ccd6964d10")
+            orderPlacedEvent.skuCode shouldBe "test_skuCode"
+        }
+    }
+
+    describe("OrderPlacedEvent - setters") {
+        it("should be able to change Id attribute of the order placed event") {
+            val orderPlacedEvent = createTestOrderPlacedEvent()
+            orderPlacedEvent.id = UUID.fromString("bbb8a937-0504-4468-823a-04ccd6964d10")
+
+            orderPlacedEvent.id shouldBe UUID.fromString("bbb8a937-0504-4468-823a-04ccd6964d10")
+        }
+
+        it("should be able to change SkuCode attribute of the order placed event") {
+            val orderPlacedEvent = createTestOrderPlacedEvent()
+            orderPlacedEvent.skuCode = "another skuCode"
+
+            orderPlacedEvent.skuCode shouldBe "another skuCode"
+        }
+    }
+})
+
+fun createTestOrderPlacedEvent(): OrderPlacedEvent = OrderPlacedEvent(
+    id = UUID.fromString("aaa8a937-0504-4468-823a-04ccd6964d10"),
+    skuCode = "test_skuCode"
+)
