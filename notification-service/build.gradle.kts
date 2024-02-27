706cad2de633349c32a5a277b22b41ef3565d086
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-02-27 22:03:45 +0530

    [Notification-service] Add. Eureka client dependency

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.3"
+	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
+
+	kotlin("jvm") version "1.9.20"
+	kotlin("plugin.spring") version "1.9.20"
 }
 
 group = "com.ecommerce"
@@ -19,10 +20,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.fasterxml.jackson.core:jackson-databind")
 	implementation("org.springframework.kafka:spring-kafka:3.1.2")
+
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.3")
 
 	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
