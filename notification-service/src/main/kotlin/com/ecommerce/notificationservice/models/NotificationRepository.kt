a667837115d94f44d6030dae5d782a8bfbd99c09
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-02 23:31:52 +0530

    [Notification-service] Add. Custom query to fetch recent notification

--- notification-service/src/main/kotlin/com/ecommerce/notificationservice/models/NotificationRepository.kt
+++ notification-service/src/main/kotlin/com/ecommerce/notificationservice/models/NotificationRepository.kt
@@ -1,6 +1,10 @@
 package com.ecommerce.notificationservice.models
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
-interface NotificationRepository : JpaRepository<Notification, UUID>
+interface NotificationRepository : JpaRepository<Notification, UUID> {
+    @Query(value = "SELECT * FROM notifications ORDER BY created_at DESC LIMIT 1", nativeQuery = true)
+    fun findRecentNotification(): List<Notification>
+}
