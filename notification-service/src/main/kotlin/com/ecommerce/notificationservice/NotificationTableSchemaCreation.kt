b2a6e329cf1c3273eb5e1e670397dede1343b7b1
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-02 21:47:37 +0530

    [Notification-service] Add. Logic to execute create notifications table sql schema

---
+++ notification-service/src/main/kotlin/com/ecommerce/notificationservice/NotificationTableSchemaCreation.kt
@@ -0,0 +1,25 @@
+package com.ecommerce.notificationservice
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.core.io.ClassPathResource
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Component
+import org.springframework.util.FileCopyUtils
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+
+@Component
+class NotificationTableSchemaCreation(private val jdbcTemplate: JdbcTemplate) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        val notificationsTablePathResource = ClassPathResource("create-notifications-table.sql")
+
+        val notificationsTableInputStreamReader = InputStreamReader(
+            notificationsTablePathResource.inputStream,
+            StandardCharsets.UTF_8
+        )
+
+        val notificationsTableQuery = FileCopyUtils.copyToString(notificationsTableInputStreamReader)
+
+        jdbcTemplate.execute(notificationsTableQuery)
+    }
+}
