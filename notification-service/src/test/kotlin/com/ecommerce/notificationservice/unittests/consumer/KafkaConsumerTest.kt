845779ee0587f99d02c4806232f0b3f742310df6
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-02 22:15:11 +0530

    [Notification-service] Add. Unit tests for the logic of persisting order placed event

--- notification-service/src/test/kotlin/com/ecommerce/notificationservice/unittests/consumer/KafkaConsumerTest.kt
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/unittests/consumer/KafkaConsumerTest.kt
@@ -1,15 +1,25 @@
 package com.ecommerce.notificationservice.unittests.consumer
 
 import com.ecommerce.notificationservice.consumer.KafkaConsumer
+import com.ecommerce.notificationservice.events.OrderPlacedEvent
+import com.ecommerce.notificationservice.models.Notification
+import com.ecommerce.notificationservice.models.NotificationRepository
 import com.ecommerce.notificationservice.utils.EntityUtils.getMethodAnnotations
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
+import java.sql.Timestamp
+import java.time.Instant
+import java.util.UUID
 
 class KafkaConsumerTest : DescribeSpec({
-    val kafkaConsumer = KafkaConsumer()
+    val mockNotificationRepository = mockk<NotificationRepository>()
+    val kafkaConsumer = KafkaConsumer(mockNotificationRepository)
 
     describe("Kafka Consumer - annotations") {
         it("should have Component annotation to the kafka consumer class") {
@@ -17,6 +27,18 @@ class KafkaConsumerTest : DescribeSpec({
             val componentAnnotation = annotations.firstOrNull { it is Component } as Component
 
             componentAnnotation shouldNotBe null
+        }
+    }
+
+    describe("handleOrderPlacedEvent") {
+        it("should be able to save order placed events to notifications table") {
+            val orderPlacedEvent = createTestOrderPlacedEvent()
+            val notification = createTestNotification()
+            every { mockNotificationRepository.save(any(Notification::class)) } returns notification
+
+            kafkaConsumer.handleOrderPlacedEvent(orderPlacedEvent)
+
+            verify { mockNotificationRepository.save(any(Notification::class)) }
         }
     }
 
@@ -36,3 +58,19 @@ class KafkaConsumerTest : DescribeSpec({
         }
     }
 })
+
+private fun createTestNotification() = Notification(
+    id = UUID.randomUUID(),
+    sender = "test@gmail.com",
+    recipient = "test@gmail.com",
+    isSent = false,
+    orderId = UUID.randomUUID(),
+    skuCode = "test_skucode",
+    createdAt = Timestamp.from(Instant.now())
+)
+
+private fun createTestOrderPlacedEvent(): OrderPlacedEvent =
+    OrderPlacedEvent(
+        id = UUID.randomUUID(),
+        skuCode = "test_skucode"
+    )
