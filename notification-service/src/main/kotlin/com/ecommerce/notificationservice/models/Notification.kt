3bbca1e2461adb890b8577ed2a638f58efefc577
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-02 21:49:16 +0530

    [Notification-service] Add. Notification & repository entity

---
+++ notification-service/src/main/kotlin/com/ecommerce/notificationservice/models/Notification.kt
@@ -0,0 +1,24 @@
+package com.ecommerce.notificationservice.models
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.sql.Timestamp
+import java.util.Date
+import java.util.UUID
+
+@Entity
+@Table(name = "notifications")
+data class Notification(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UUID?,
+    var sender: String,
+    var recipient: String,
+    var isSent: Boolean,
+    var orderId: UUID,
+    var skuCode: String,
+    var createdAt: Timestamp
+)
