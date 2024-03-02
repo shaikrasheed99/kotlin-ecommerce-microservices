c9cbfce40e6210a777b85be937c23b225485f9a8
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-02 21:50:32 +0530

    [Notification-service] Add. Logic to persist order placed events to notifications table

--- notification-service/src/main/kotlin/com/ecommerce/notificationservice/consumer/KafkaConsumer.kt
+++ notification-service/src/main/kotlin/com/ecommerce/notificationservice/consumer/KafkaConsumer.kt
@@ -1,19 +1,37 @@
 package com.ecommerce.notificationservice.consumer
 
 import com.ecommerce.notificationservice.events.OrderPlacedEvent
+import com.ecommerce.notificationservice.models.Notification
+import com.ecommerce.notificationservice.models.NotificationRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
+import java.sql.Timestamp
+import java.time.Instant
 
 @Component
-class KafkaConsumer {
+class KafkaConsumer(private val notificationRepository: NotificationRepository) {
     @KafkaListener(topics = ["notificationsTopic"])
     fun handleOrderPlacedEvent(orderPlacedEvent: OrderPlacedEvent) {
         logger.info(
             "Received event with order id: ${orderPlacedEvent.id} and skucode: ${orderPlacedEvent.skuCode}"
         )
+
+        val notification = mapToNotification(orderPlacedEvent)
+        notificationRepository.save(notification)
     }
+
+    private fun mapToNotification(orderPlacedEvent: OrderPlacedEvent): Notification =
+        Notification(
+            id = null,
+            sender = "ecommerce.microservices@gmail.com",
+            recipient = "test@gmail.com",
+            isSent = false,
+            orderId = orderPlacedEvent.id,
+            skuCode = orderPlacedEvent.skuCode,
+            createdAt = Timestamp.from(Instant.now())
+        )
 
     companion object {
         val logger: Logger = LoggerFactory.getLogger(this::class.java)
