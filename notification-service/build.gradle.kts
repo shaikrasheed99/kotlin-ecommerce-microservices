bafeb07d50749afa9c14b9730b1026470015e076
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-02 22:13:24 +0530

    [Notification-service] Add. Mockk dependency

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
 
 	runtimeOnly("org.postgresql:postgresql")
 
+	testImplementation("io.mockk:mockk:1.13.9")
+
 	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 	testImplementation("io.kotest:kotest-assertions-json:4.6.3")
