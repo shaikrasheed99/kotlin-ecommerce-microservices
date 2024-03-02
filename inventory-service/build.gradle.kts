fd9699ed5905a61d35c922767307f022e62cab51
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-02 23:06:48 +0530

    [Inventory-service] Fix. InventoryServiceApplication test by adding h2 database dependency

--- inventory-service/build.gradle.kts
+++ inventory-service/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 
 	testImplementation("io.mockk:mockk:1.13.9")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2")
 
 	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
--- inventory-service/src/test/kotlin/com/ecommerce/inventoryservice/InventoryServiceApplicationTests.kt
+++ inventory-service/src/test/kotlin/com/ecommerce/inventoryservice/InventoryServiceApplicationTests.kt
@@ -1,21 +1,10 @@
 package com.ecommerce.inventoryservice
 
-import com.ecommerce.inventoryservice.utils.TestUtils.getPostgreSQLContainer
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
-@Testcontainers
 class InventoryServiceApplicationTests {
-
-    companion object {
-        @Container
-        @ServiceConnection
-        val postgreSQLContainer = getPostgreSQLContainer()
-    }
 
     @Test
     fun contextLoads() {
