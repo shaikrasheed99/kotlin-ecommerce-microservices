38331711d4fbb25f5a563f327568eccd59147e60
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-04 19:31:14 +0530

    [Notification-service] Add. Testcontainers dependency

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -47,6 +47,10 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
+
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
