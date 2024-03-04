c65401e3fe62e72ffddcc0a0b6bd4dc55f47db35
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-04 23:42:59 +0530

    [Order-service] Revert. Test coverage to 100 percentage

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -106,7 +106,7 @@ tasks.jacocoTestReport {
 
 tasks.jacocoTestCoverageVerification {
     dependsOn(tasks.test)
-    val minimumThreadValue = 0.90.toBigDecimal()
+    val minimumThreadValue = 0.99.toBigDecimal()
     violationRules {
         rule {
             element = "CLASS"
