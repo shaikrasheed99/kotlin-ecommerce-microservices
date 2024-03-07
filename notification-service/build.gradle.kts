fc703e3e9c4a96ec9200fda9328c9114c9f3d3ae
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-08 00:24:35 +0530

    [Notification-service] Add. Awaitility dependency

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.awaitility:awaitility:4.2.0")
 }
 
 tasks.withType<KotlinCompile> {
