ebbbea1759405bef3db4131dc99ae1e4201063bd
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-03-08 00:25:00 +0530

    [Notification-service] Add. Integration test for kafka consumer

---
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/integrationtests/consumer/KafkaConsumerIntegrationTest.kt
@@ -0,0 +1,56 @@
+package com.ecommerce.notificationservice.integrationtests.consumer
+
+import com.ecommerce.notificationservice.events.OrderPlacedEvent
+import com.ecommerce.notificationservice.models.NotificationRepository
+import com.ecommerce.notificationservice.utils.EmbeddedKafkaProducerTestUtils.createTestProducer
+import com.ecommerce.notificationservice.utils.TestUtils.createTestOrderPlacedEvent
+import io.kotest.matchers.shouldBe
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.awaitility.Awaitility.await
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.EmbeddedKafkaBroker
+import org.springframework.kafka.test.context.EmbeddedKafka
+import java.time.Duration
+
+const val DEFAULT_TEST_TOPIC = "testTopic"
+
+@SpringBootTest
+@EmbeddedKafka
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class KafkaConsumerIntegrationTest {
+    @Autowired
+    private lateinit var embeddedKafkaBroker: EmbeddedKafkaBroker
+
+    @Autowired
+    private lateinit var notificationRepository: NotificationRepository
+
+    private lateinit var testProducer: Producer<String, OrderPlacedEvent>
+
+    @BeforeEach
+    fun setUp() {
+        testProducer = createTestProducer(embeddedKafkaBroker)
+        notificationRepository.deleteAll()
+    }
+
+    @Test
+    fun shouldBeAbleToConsumeOrderPlacedEvents() {
+        val orderPlacedEvent = createTestOrderPlacedEvent()
+        val testKey = "testKey"
+
+        testProducer.send(ProducerRecord(DEFAULT_TEST_TOPIC, testKey, orderPlacedEvent))
+
+        await().atMost(Duration.ofSeconds(30)).untilAsserted {
+            val notification = notificationRepository.findRecentNotification()
+
+            if (notification.isNotEmpty()) {
+                notification[0].orderId shouldBe orderPlacedEvent.id
+                notification[0].skuCode shouldBe orderPlacedEvent.skuCode
+            }
+        }
+    }
+}
---
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/utils/EmbeddedKafkaProducerTestUtils.kt
@@ -0,0 +1,27 @@
+package com.ecommerce.notificationservice.utils
+
+import com.ecommerce.notificationservice.events.OrderPlacedEvent
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+import org.springframework.kafka.test.EmbeddedKafkaBroker
+import org.springframework.kafka.test.utils.KafkaTestUtils
+
+object EmbeddedKafkaProducerTestUtils {
+    fun createTestProducer(embeddedKafkaBroker: EmbeddedKafkaBroker): Producer<String, OrderPlacedEvent> {
+        val producerProps = KafkaTestUtils.producerProps(embeddedKafkaBroker)
+        addKeyAndValueSerializers(producerProps)
+
+        val producerFactory = DefaultKafkaProducerFactory<String, OrderPlacedEvent>(producerProps)
+        val producer = producerFactory.createProducer()
+
+        return producer
+    }
+
+    private fun addKeyAndValueSerializers(producerProps: MutableMap<String, Any>) {
+        producerProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        producerProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
+    }
+}
