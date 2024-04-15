6a5f348a7262bbe94a7d2d5c79c9b8c9233fc092
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-04-16 00:29:58 +0530

    [Notification-service] Fix. Unit tests of event consumer

--- notification-service/src/test/kotlin/com/ecommerce/notificationservice/unittests/consumer/EventConsumerTest.kt
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/unittests/consumer/EventConsumerTest.kt
@@ -3,9 +3,11 @@ package com.ecommerce.notificationservice.unittests.consumer
 import com.ecommerce.notificationservice.consumer.EventConsumer
 import com.ecommerce.notificationservice.models.Notification
 import com.ecommerce.notificationservice.models.NotificationRepository
+import com.ecommerce.notificationservice.utils.EmbeddedKafkaProducerTestUtils.createAndSerializeCloudEvent
 import com.ecommerce.notificationservice.utils.EntityUtils.getMethodAnnotations
 import com.ecommerce.notificationservice.utils.TestUtils.createTestNotification
 import com.ecommerce.notificationservice.utils.TestUtils.createTestOrderPlacedEvent
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
@@ -15,11 +17,17 @@ import io.mockk.verify
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
+private const val TEST_TOPIC = "testTopic"
+private const val ORDER_PLACED_EVENT_TYPE = "order_placed"
+private const val TEST_SOURCE = "test_source"
+
 class EventConsumerTest : DescribeSpec({
     val mockNotificationRepository = mockk<NotificationRepository>()
     val eventConsumer = EventConsumer(mockNotificationRepository)
 
-    describe("Kafka Consumer - annotations") {
+    val mapper = ObjectMapper()
+
+    describe("Event Consumer - annotations") {
         it("should have Component annotation to the event consumer class") {
             val annotations = eventConsumer.javaClass.annotations
             val componentAnnotation = annotations.firstOrNull { it is Component } as Component
@@ -28,21 +36,26 @@ class EventConsumerTest : DescribeSpec({
         }
     }
 
-    describe("handleOrderPlacedEvent") {
+    describe("consume") {
         it("should be able to save order placed events to notifications table") {
-            val orderPlacedEvent = createTestOrderPlacedEvent()
+            val orderPlacedEventJson = mapper.writeValueAsString(createTestOrderPlacedEvent())
+            val payload = createAndSerializeCloudEvent(
+                event = orderPlacedEventJson,
+                eventType = ORDER_PLACED_EVENT_TYPE,
+                source = TEST_SOURCE
+            )
             val notification = createTestNotification()
             every { mockNotificationRepository.save(any(Notification::class)) } returns notification
 
-            eventConsumer.handleOrderPlacedEvent(orderPlacedEvent)
+            eventConsumer.consume(payload, TEST_TOPIC)
 
             verify { mockNotificationRepository.save(any(Notification::class)) }
         }
     }
 
-    describe("handleOrderPlacedEvent - annotations") {
-        it("should have KafkaListener annotation to the handleOrderPlacedEvent method") {
-            val methodAnnotations = eventConsumer.getMethodAnnotations("handleOrderPlacedEvent")
+    describe("consume - annotations") {
+        it("should have KafkaListener annotation to the consume method") {
+            val methodAnnotations = eventConsumer.getMethodAnnotations("consume")
             val kafkaListenerAnnotation = methodAnnotations.firstOrNull { it is KafkaListener } as KafkaListener
 
             kafkaListenerAnnotation shouldNotBe null
@@ -50,7 +63,7 @@ class EventConsumerTest : DescribeSpec({
         }
     }
 
-    describe("Kafka Consumer - logger") {
+    describe("Event Consumer - logger") {
         it("should initialize the logger of event consumer") {
             EventConsumer.logger shouldNotBe null
         }
--- notification-service/src/test/kotlin/com/ecommerce/notificationservice/utils/EmbeddedKafkaProducerTestUtils.kt
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/utils/EmbeddedKafkaProducerTestUtils.kt
@@ -1,20 +1,29 @@
 package com.ecommerce.notificationservice.utils
 
 import com.ecommerce.notificationservice.events.OrderPlacedEvent
+import io.cloudevents.CloudEvent
+import io.cloudevents.core.builder.CloudEventBuilder
+import io.cloudevents.core.provider.EventFormatProvider
+import io.cloudevents.jackson.JsonFormat
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.http.MediaType
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.support.serializer.JsonSerializer
 import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.utils.KafkaTestUtils
+import java.net.URI
+import java.nio.charset.StandardCharsets
+import java.time.OffsetDateTime
+import java.util.UUID
 
 object EmbeddedKafkaProducerTestUtils {
-    fun createTestProducer(embeddedKafkaBroker: EmbeddedKafkaBroker): Producer<String, OrderPlacedEvent> {
+    fun createTestProducer(embeddedKafkaBroker: EmbeddedKafkaBroker): Producer<String, String> {
         val producerProps = KafkaTestUtils.producerProps(embeddedKafkaBroker)
         addAdditionalProperties(embeddedKafkaBroker, producerProps)
 
-        val producerFactory = DefaultKafkaProducerFactory<String, OrderPlacedEvent>(producerProps)
+        val producerFactory = DefaultKafkaProducerFactory<String, String>(producerProps)
         val producer = producerFactory.createProducer()
 
         return producer
@@ -26,6 +35,32 @@ object EmbeddedKafkaProducerTestUtils {
     ) {
         producerProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = embeddedKafkaBroker.brokersAsString
         producerProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        producerProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
+        producerProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+    }
+
+    fun createAndSerializeCloudEvent(event: String, eventType: String, source: String): String {
+        val cloudEvent = createCloudEvent(event, eventType, source)
+        val message = serializeCloudEvent(cloudEvent)
+
+        return message
+    }
+
+    private fun createCloudEvent(event: String, eventType: String, source: String): CloudEvent {
+        val currentTime = OffsetDateTime.now()
+
+        return CloudEventBuilder.v1()
+            .withId(UUID.randomUUID().toString())
+            .withType(eventType)
+            .withTime(currentTime)
+            .withSource(URI.create(source))
+            .withData(MediaType.APPLICATION_JSON.toString(), event.toByteArray())
+            .build()
+    }
+
+    private fun serializeCloudEvent(cloudEvent: CloudEvent): String {
+        return EventFormatProvider
+            .getInstance()
+            .resolveFormat(JsonFormat.CONTENT_TYPE)!!
+            .serialize(cloudEvent).toString(StandardCharsets.UTF_8)
     }
 }
