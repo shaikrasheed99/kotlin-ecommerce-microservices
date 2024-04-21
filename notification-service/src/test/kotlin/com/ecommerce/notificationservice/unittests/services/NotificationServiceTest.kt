400453d956bd96b06da3ba3e5b3b61de1bf97f8e
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-04-21 16:41:09 +0530

    [Notification-service] Fix. Detekt formatting issues

--- notification-service/src/test/kotlin/com/ecommerce/notificationservice/unittests/services/NotificationServiceTest.kt
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/unittests/services/NotificationServiceTest.kt
@@ -9,7 +9,6 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import io.kotest.matchers.types.shouldBeSameInstanceAs
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
--- notification-service/src/test/kotlin/com/ecommerce/notificationservice/utils/EmbeddedKafkaProducerTestUtils.kt
+++ notification-service/src/test/kotlin/com/ecommerce/notificationservice/utils/EmbeddedKafkaProducerTestUtils.kt
@@ -1,6 +1,5 @@
 package com.ecommerce.notificationservice.utils
 
-import com.ecommerce.notificationservice.events.OrderPlacedEvent
 import io.cloudevents.CloudEvent
 import io.cloudevents.core.builder.CloudEventBuilder
 import io.cloudevents.core.provider.EventFormatProvider
@@ -10,7 +9,6 @@ import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.http.MediaType
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.support.serializer.JsonSerializer
 import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.utils.KafkaTestUtils
 import java.net.URI
