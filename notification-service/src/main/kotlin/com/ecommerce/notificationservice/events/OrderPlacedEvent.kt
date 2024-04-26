c44397476da27ada26e7e86090b51298cd1392fd
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-04-26 22:56:21 +0530

    [Notification-service] Add. JsonIgnoreProperty annotation to order placed event entity

--- notification-service/src/main/kotlin/com/ecommerce/notificationservice/events/OrderPlacedEvent.kt
+++ notification-service/src/main/kotlin/com/ecommerce/notificationservice/events/OrderPlacedEvent.kt
@@ -1,8 +1,10 @@
 package com.ecommerce.notificationservice.events
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class OrderPlacedEvent(
     @JsonProperty("orderId")
     var orderId: UUID,
