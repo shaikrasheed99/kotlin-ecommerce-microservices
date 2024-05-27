58fb267bf403b67b04f3235b71deda5397a4efd4
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-05-27 16:42:37 +0530

    [Discovery-server] Add. Buildpack configuration to create docker image

--- discovery-server/build.gradle.kts
+++ discovery-server/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
 	id("org.springframework.boot") version "3.2.0"
@@ -43,4 +44,16 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+springBoot {
+	buildInfo()
+}
+
+tasks.named<BootBuildImage>("bootBuildImage") {
+	imageName = "shaikrasheed99/discovery-server:latest"
+	builder = "paketobuildpacks/builder:base"
+	environment = mapOf(
+		"BP_JVM_VERSION" to "17"
+	)
 }
