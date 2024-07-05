a6350720f98b18a0a5c89669519f452bcf5136af
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-07-05 11:39:43 +0530

    [Order-service] Refactor. Deprecated kotlinOptions to compilerOptions

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 import org.springframework.boot.gradle.tasks.bundling.BootJar
@@ -66,9 +67,9 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
