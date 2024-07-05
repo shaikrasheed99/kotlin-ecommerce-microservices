66ee71efa5b037d3456445359e5e9403c168c61e
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-07-05 11:18:58 +0530

    [Inventory-service] Remove. SERVER_UP constant value from MessageResponses

--- inventory-service/src/main/kotlin/com/ecommerce/inventoryservice/constants/MessageResponses.kt
+++ inventory-service/src/main/kotlin/com/ecommerce/inventoryservice/constants/MessageResponses.kt
@@ -1,7 +1,6 @@
 package com.ecommerce.inventoryservice.constants
 
 enum class MessageResponses(val message: String) {
-    SERVER_UP("Server is up!!"),
     INVENTORY_FETCHED_SUCCESS("Inventory details fetched successfully"),
     INVENTORY_NOT_FOUND("Inventory not found")
 }
