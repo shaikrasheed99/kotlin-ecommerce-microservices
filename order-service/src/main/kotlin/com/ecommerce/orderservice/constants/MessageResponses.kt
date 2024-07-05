c82c0d5d10339f4b2166dc7605095e3d03bbf3b1
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-07-05 10:44:44 +0530

    [Order-service] Remove. SERVER_UP constant value from MessageResponses

--- order-service/src/main/kotlin/com/ecommerce/orderservice/constants/MessageResponses.kt
+++ order-service/src/main/kotlin/com/ecommerce/orderservice/constants/MessageResponses.kt
@@ -1,6 +1,5 @@
 package com.ecommerce.orderservice.constants
 
 enum class MessageResponses(val message: String) {
-    SERVER_UP("Server is up!!"),
     ORDER_CREATION_SUCCESS("Order has created successfully"),
 }
