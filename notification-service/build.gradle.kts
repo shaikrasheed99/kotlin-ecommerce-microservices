0385b6a279d52d5e7c3e1819418164651d49f59c
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-07-06 14:50:46 +0530

    [Notification-service] Refactor. Dependencies version to variables

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -24,31 +24,41 @@ repositories {
 	mavenCentral()
 }
 
+val kotlinXJsonVersion = "1.2.2"
+val eurekaClientVersion = "4.1.2"
+val cloudEventsJsonVersion = "4.0.1"
+val detektVersion = "1.23.1"
+val mockkVersion = "1.13.9"
+val kotestJunit5Version = "5.6.2"
+val kotestSpringVersion = "1.1.3"
+val kotestJsonVersion = "4.6.3"
+val awaitilityVersion = "4.2.0"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinXJsonVersion")
 	implementation("com.fasterxml.jackson.core:jackson-databind")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 	implementation("org.springframework.kafka:spring-kafka")
-	implementation("io.cloudevents:cloudevents-json-jackson:4.0.1")
+	implementation("io.cloudevents:cloudevents-json-jackson:$cloudEventsJsonVersion")
 
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.1.2")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:$eurekaClientVersion")
 
-	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
 
 	runtimeOnly("org.postgresql:postgresql")
 
-	testImplementation("io.mockk:mockk:1.13.9")
+	testImplementation("io.mockk:mockk:$mockkVersion")
 	testImplementation("com.h2database:h2")
 
-	testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
-	testImplementation("io.kotest:kotest-assertions-json:4.6.3")
+	testImplementation("io.kotest:kotest-runner-junit5:$kotestJunit5Version")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestSpringVersion")
+	testImplementation("io.kotest:kotest-assertions-json:$kotestJsonVersion")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
@@ -56,7 +66,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
-	testImplementation("org.awaitility:awaitility:4.2.0")
+	testImplementation("org.awaitility:awaitility:$awaitilityVersion")
 }
 
 tasks.withType<KotlinCompile> {
