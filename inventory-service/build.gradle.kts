9f3a2f80c8c522725da90d21689a0464be334bfe
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-07-16 23:44:31 +0530

    [Inventory-service] Upgrade. JPA & Kotlin spring plugin versions

--- inventory-service/build.gradle.kts
+++ inventory-service/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 	id("io.gitlab.arturbosch.detekt") version "1.23.6"
 
 	kotlin("jvm") version "2.0.0"
-	kotlin("plugin.spring") version "1.9.20"
-	kotlin("plugin.jpa") version "1.9.20"
+	kotlin("plugin.spring") version "2.0.0"
+	kotlin("plugin.jpa") version "2.0.0"
 	jacoco
 }
 
