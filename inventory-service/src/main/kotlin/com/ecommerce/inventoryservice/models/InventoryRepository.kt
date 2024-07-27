c1b424ca29e639047316301c09d013f8034cc5a0
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-07-27 06:11:14 +0530

    [Inventory-service] Refactor. Custom repository query from native to JPQL

--- inventory-service/src/main/kotlin/com/ecommerce/inventoryservice/models/InventoryRepository.kt
+++ inventory-service/src/main/kotlin/com/ecommerce/inventoryservice/models/InventoryRepository.kt
@@ -2,11 +2,12 @@ package com.ecommerce.inventoryservice.models
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface InventoryRepository : JpaRepository<Inventory, Int> {
-    @Query(value = "SELECT * from inventory where sku_code = :skuCode", nativeQuery = true)
-    fun findInventoryBySkuCode(skuCode: String): Optional<Inventory>
+    @Query(value = "SELECT i FROM Inventory as i WHERE i.skuCode = :skuCode")
+    fun findInventoryBySkuCode(@Param("skuCode") skuCode: String): Optional<Inventory>
 }
