5adf388ca98d6ccb8125df9059e52a7577b568dd
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-09-08 13:47:54 +0530

    [Inventory-service] Add. Logic to process the event after checking in inbox table for idempotent nature

--- inventory-service/src/main/kotlin/com/ecommerce/inventoryservice/consumer/EventConsumer.kt
+++ inventory-service/src/main/kotlin/com/ecommerce/inventoryservice/consumer/EventConsumer.kt
@@ -3,19 +3,26 @@ package com.ecommerce.inventoryservice.consumer
 import com.ecommerce.inventoryservice.constants.EventTypes.ORDER_PLACED
 import com.ecommerce.inventoryservice.events.EventData
 import com.ecommerce.inventoryservice.events.OrderPlacedEvent
+import com.ecommerce.inventoryservice.models.inbox.Inbox
+import com.ecommerce.inventoryservice.models.inbox.InboxRepository
 import com.ecommerce.inventoryservice.services.InventoryService
 import com.ecommerce.inventoryservice.utils.CloudEventProcessor.deserializeCloudEvent
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.cloudevents.CloudEvent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
-class EventConsumer(private val inventoryService: InventoryService) {
+class EventConsumer(
+    private val inventoryService: InventoryService,
+    private val inboxRepository: InboxRepository
+) {
     @KafkaListener(topics = ["\${spring.kafka.topic}"])
     fun consume(
         payload: String,
@@ -25,10 +32,25 @@ class EventConsumer(private val inventoryService: InventoryService) {
 
         val cloudEvent = deserializeCloudEvent(payload)
 
-        when (cloudEvent.type) {
-            ORDER_PLACED.type -> handleOrderPlacedEvent(payload)
+        val inbox = inboxRepository.findByEventId(UUID.fromString(cloudEvent.id))
+
+        if (inbox.isEmpty) {
+            when (cloudEvent.type) {
+                ORDER_PLACED.type -> handleOrderPlacedEvent(payload)
+            }
+
+            inboxRepository.save(buildInbox(cloudEvent, topic))
+        } else {
+            logger.info("Event with id: ${cloudEvent.id} is already consumed, hence skipping it")
         }
     }
+
+    private fun buildInbox(cloudEvent: CloudEvent, topic: String) = Inbox(
+        id = UUID.randomUUID(),
+        eventId = UUID.fromString(cloudEvent.id),
+        eventType = cloudEvent.type,
+        topic = topic
+    )
 
     private fun handleOrderPlacedEvent(payload: String) {
         val orderPlacedEvent = deserializeOrderPlacedEvent(payload)
